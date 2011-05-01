ea1dba934f3127b5743ba7fea8f62706a3669587
alafroiskiotos <praetorian@akyroi.gr>
2011-05-01 21:25:35 +0300

    Javadoc of doublylinked list and fifo queue

--- src/structures/DoublyLinkedList.java
+++ src/structures/DoublyLinkedList.java
@@ -1,35 +1,91 @@
 package structures;
 
+/**
+ * Κλάση που αντιπροσωπεύει ένα κόμβο στη διπλά συνδεδεμένη λίστα.
+ * Κρατάει την τιμή του κόμβου, τον επόμενο και τον προηγούμενο
+ * 
+ * @param <E> Ο τύπος των δεδομένων που θα αποθηκεύονται στο κόμβο
+ */
 class DNode<E>{
 	private E value;
 	private DNode<E> previousNode;
 	private DNode<E> nextNode;
 	
+	/**
+	 * Ο constructor με τον οποίο ορίζουμε την
+	 * τιμή του κόμβου
+	 * 
+	 * @param value Η τιμή του κόμβου
+	 */
 	DNode(E value){
 		this.value=value;
 	}
+	
+	/**
+	 * Ορίζουμε τον προηγούμενο κόμβο στη λίστα
+	 * 
+	 * @param previousNode Ο προηγούμενος κόμβος στη λίστα
+	 */
 	void setPreviousNode(DNode<E> previousNode){
 		this.previousNode=previousNode;
 	}
+	
+	/**
+	 * Ορίζουμε τον επόμενο κόμβο στη λίστα
+	 * 
+	 * @param nextNode Ο επόμενος κόμβος στη λίστα
+	 */
 	void setNextNode(DNode<E> nextNode){
 		this.nextNode=nextNode;
 	}
+	
+	/**
+	 * Παίρνουμε την τιμή του κόμβου
+	 * 
+	 * @return Την τιμή του κόμβου
+	 */
 	E getValue(){
 		return value;
 	}
+	
+	/**
+	 * Παίρνουμε τον προηγούμενο κόμβο από τη λίστα
+	 * 
+	 * @return Τον προηγούμενο κόμβο από τη λίστα
+	 */
 	DNode<E> getPreviousNode(){
 		return previousNode;
 	}
+	
+	/**
+	 * Παίρνουμε τον επόμενο κόμβο από τη λίστα
+	 * 
+	 * @return Τον επόμενο κόμβο από τη λίστα
+	 */
 	DNode<E> getNextNode(){
 		return nextNode;
 	}
 }
 
+/**
+ * Κλάση που υλοποιεί την διπλά συνδεδεμένη λίστα
+ *
+ * @param <E> Τον τύπο δεδομένων που θα χειρίζεται η λίστα
+ */
 public class DoublyLinkedList<E> {
+	//Δύο βοηθητικοί κόμβοι που αντιπροσωπεύουν
+	//την αρχή και το τέλος της λίστας
 	private DNode<E> head=new DNode<E>(null);
 	private DNode<E> tail=new DNode<E>(null);
+	//Το μήκος της λίστας
 	private int length=0;
 	
+	/**
+	 * Constructor με τον οποίο ορίζουμε ότι ο προηγούμενος
+	 * κόμβος του πρώτου είναι το null, ο επόμενός του είναι
+	 * ο τελευταίος, ο προηγούμενος του τελευταίου είναι ο
+	 * πρώτος και ο επόμενος του τελευταίου είναι το null
+	 */
 	public DoublyLinkedList(){
 		head.setPreviousNode(null);
 		head.setNextNode(tail);
@@ -37,12 +93,20 @@ public class DoublyLinkedList<E> {
 		tail.setNextNode(null);
 	}
 	
-	//Given an index, returns a node of the list
+	/**
+	 * Του δίνουμε ένα δείκτη και παίρνουμε τον συγκεκριμένο κόμβο
+	 * 
+	 * @param Ο δείκτης του κόμβου που μας ενδιαφέρει
+	 * @return Τον κόμβο σύμφωνα με τον δείκτη
+	 * @throws IndexOutOfBoundsException Σε περίπτωση που ο δείκτης είναι
+	 * μικρότερος του μηδέν ή μεγαλύτερος του μήκους της λίστας
+	 */
 	public DNode<E> getNode(int index) throws IndexOutOfBoundsException{
 		if(index<0 || index>length){
 			System.err.println("Index out of bounds");
 			throw new IndexOutOfBoundsException();
 		}else{
+			//Παίρνουμε τον επόμενο κόμβο μέχρι τον δείκτη που ορίσαμε
 			DNode<E> cursor=head;
 			for(int i=0;i<index;i++)
 				cursor=cursor.getNextNode();
@@ -51,14 +115,33 @@ public class DoublyLinkedList<E> {
 		}
 	}
 	
-	//Given an index, returns the value of a node from the list
+	/**
+	 * Παίρνουμε την τιμή ενός κόμβου χωρίς όμως να τον αφαιρέσουμε από τη λίστα
+	 * 
+	 * @param index Ο δείκτης για τον κόμβο που θέλουμε
+	 * @return Την τιμή του κόμβου που θέλουμε
+	 * @see DoublyLinkedList#getNode(int)
+	 */
 	public E getNodeValue(int index){
 		DNode<E> cursor=getNode(index);
 		
 		return cursor.getValue();
 	}
 	
-	//Given an index, removes a node from the list
+	/**
+	 * Αφαιρεί ένα συγκεκριμένο κόμβο από τη λίστα και παίρνουμε την τιμή του
+	 * σύμφωνα με τον δείκτη που δώσαμε
+	 * 
+	 * @param index Ο δείκτης για τον κόμβο που θέλουμε
+	 * @return Την τίμη του κόμβου που θέλουμε
+	 * @throws IndexOutOfBoundsException Σε περίπτωση που ο δείκτης είναι μικρότερος
+	 * του μηδέν ή μεγαλύτερος του μήκους της λίστας
+	 * @see DoublyLinkedList#getNode(int)
+	 * @see DNode#getNextNode()
+	 * @see DNode#setPreviousNode(DNode)
+	 * @see DNode#getPreviousNode()
+	 * @see DNode#setNextNode(DNode)
+	 */
 	public E removeNode(int index) throws IndexOutOfBoundsException{
 		if(index<0 || index>length){
 			System.err.println("Index out of bounds");
@@ -73,7 +156,17 @@ public class DoublyLinkedList<E> {
 		}
 	}
 	
-	//Adds a new node somewhere in the middle
+	/**
+	 * Προσθέτει ένα κόμβο στη λίστα σε συγκεκριμένη θέση
+	 * 
+	 * @param index Η θέση που θέλουμε να βάλουμε τον κόμβο
+	 * @param value Η τιμή που θέλουμε να έχει ο κόμβος
+	 * @see DoublyLinkedList#getNode(int)
+	 * @see DNode#DNode(Object)
+	 * @see DNode#setPreviousNode(DNode)
+	 * @see DNode#setNextNode(DNode)
+	 * @see DNode#getNextNode()
+	 */
 	public void add(int index, E value){
 		DNode<E> cursor=getNode(index);
 		DNode<E> newNode=new DNode<E>(value);
@@ -84,7 +177,15 @@ public class DoublyLinkedList<E> {
 		length++;
 	}
 	
-	//Adds a new node in the front
+	/**
+	 * Προσθέτει ένα νέο κόμβο στην αρχή της λίστας
+	 * 
+	 * @param value Η τιμή που θέλουμε να έχει ο νέος κόμβος
+	 * @see DNode#DNode(Object)
+	 * @see DNode#setPreviousNode(DNode)
+	 * @see DNode#setNextNode(DNode)
+	 * @see DNode#getNextNode()
+	 */
 	public void addHead(E value){
 		DNode<E> cursor=head;
 		DNode<E> newNode=new DNode<E>(value);
@@ -95,7 +196,16 @@ public class DoublyLinkedList<E> {
 		length++;
 	}
 	
-	//Adds a new node at the back
+	/**
+	 * Προσθέτει ένα νέο κόμβο στο τέλος της λίστας
+	 * 
+	 * @param value Η τιμή που θέλουμε να έχει ο νέος κόμβος
+	 * @see DNode#DNode(Object)
+	 * @see DNode#getPreviousNode()
+	 * @see DNode#setPreviousNode(DNode)
+	 * @see DNode#getNextNode()
+	 * @see DNode#setNextNode(DNode)
+	 */
 	public void addTail(E value){
 		DNode<E> cursor=tail.getPreviousNode();
 		DNode<E> newNode=new DNode<E>(value);
@@ -106,18 +216,31 @@ public class DoublyLinkedList<E> {
 		length++;
 	}
 	
-	//Returns the length of the list
+	/**
+	 * Μας επιστρέφει το μήκος της λίστας
+	 * 
+	 * @return Tο μήκος της λίστας
+	 */
 	public int getLength(){
 		return length;
 	}
 	
-	//Checks if list is empty
+	/**
+	 * Ελέγχει αν η λίστα είναι άδεια
+	 * 
+	 * @return true αν η λίστα είναι άδεια, false αν
+	 * η λίστα έχει κόμβους
+	 */
 	public boolean isEmpty(){
 		return length==0?true:false;
 	}
 	
+	/**
+	 * Επιστρέφει τις τιμές κάθε κόμβου της λίστας
+	 * 
+	 * @return Tις τιμές κάθε κόμβου της λίστας
+	 */
 	@Override
-	//Print all the list
 	public String toString(){
 		StringBuffer sb=new StringBuffer();
 		sb.append("\n");
--- src/structures/FifoQueue.java
+++ src/structures/FifoQueue.java
@@ -1,39 +1,90 @@
 package structures;
 
+/**
+ * Κλάση που αντιπροσωπεύει ένα κόμβο στην ουρά αναμονής
+ * Κρατάει την τιμή του κόμβου και τον επόμενο κόμβο
+ *
+ * @param <E> Ο τύπος των δεδομένων που θα αποθηκεύονται στο κόμβο
+ */
 class QNode<E>{
 	private E value;
 	private QNode<E> nextNode;
 	
+	/**
+	 * Constructor με τον οποίο ορίζουμε την τιμή του κόμβου
+	 * 
+	 * @param value Η τίμη του κόμβου
+	 */
 	QNode(E value){
 		this.value=value;
 	}
+	
+	/**
+	 * Ορίζουμε τον επόμενο κόμβο στην ουρά
+	 * 
+	 * @param nextNode Ο επόμενος κόμβος
+	 */
 	void setNextNode(QNode<E> nextNode){
 		this.nextNode=nextNode;
 	}
+	
+	/**
+	 * Παίρνουμε την τιμή του κόμβου
+	 * 
+	 * @return Την τιμή του κόμβου
+	 */
 	E getValue(){
 		return value;
 	}
+	
+	/**
+	 * Παίρνουμε τον επόμενο κόμβο από την ουρά
+	 * 
+	 * @return Τον επόμενο κόμβο από την ουρά
+	 */
 	QNode<E> getNextNode(){
 		return nextNode;
 	}
 }
+
+/**
+ * Κλάση που υλοποιεί την ουρά αναμονής
+ *
+ * @param <E> Ο τύπος δεδομένων που θα χειρίζεται η ουρά
+ */
 public class FifoQueue<E> {
+	//Ο πρώτος και ο τελευταίος κόμβος στην ουρά
 	private QNode<E> head;
 	private QNode<E> tail;
+	//Το μήκος της ουράς
 	private int length;
 	
+	/**
+	 * Constructor που αρχικοποιεί τον πρώτο και τον τελευταίο
+	 * κόμβο σε null και το μήκος σε μηδέν
+	 */
 	public FifoQueue(){
 		head=null;
 		tail=null;
 		length=0;
 	}
-	//Push a node to the queue
-	public void enqueue(E value){
+
+	/**
+	 * Βάζει μία τιμή στην ουρά αναμονής
+	 * 
+	 * @param value Η τιμή του νέου κόμβου στην ουρά
+	 * @see QNode#QNode(Object)
+	 * @see QNode#setNextNode(QNode)
+	 */
+	public void enqueue(E value){	
 		if(head==null){
+			//Ο νέος κόμβος είναι ο πρώτος κόμβος
+			//που μπαίνει στην ουρά
 			head=new QNode<E>(value);
 			head.setNextNode(null);
 			tail=head;
 		}else{
+			//Ο νέος κόμβος μπαίνει μετά τον τελευταίο κόμβο
 			QNode<E> newNode=new QNode<E>(value);
 			newNode.setNextNode(null);
 			tail.setNextNode(newNode);
@@ -41,20 +92,39 @@ public class FifoQueue<E> {
 		}
 		length++;
 	}
-	//Pop a node from the queue
+	
+	/**
+	 * Παίρνουμε τον πρώτο κόμβο από τη FIFO ουρά και τον διαγράφουμε
+	 * 
+	 * @return Την τιμή του πρώτου κόμβου
+	 * @throws IndexOutOfBoundsException Σε περίπτωση που η λίστα είναι άδεια
+	 * @see QNode#getValue()
+	 * @see QNode#getNextNode()
+	 */
 	public E dequeue() throws IndexOutOfBoundsException{
 		if(length==0){
 			System.err.println("Queue is empty!");
 			throw new IndexOutOfBoundsException();
 		}else{
 			E value=head.getValue();
+			//Ορίζουμε ως πρώτο κόμβο, τον επόμενο
 			head=head.getNextNode();
 			length--;
 			
 			return value;
 		}
 	}
-	//Get a node from the queue without poping it
+	
+	/**
+	 * Παίρνουμε ένα συγκεκριμένο κόμβο από την ουρά σύμφωνα 
+	 * με τον δείκτη χωρίς να τον διαγράψουμε
+	 * 
+	 * @param index Ο δείκτης του κόμβου που μας ενδιαφέρει
+	 * @return Τον κόμβο σύμφωνα με το δείκτη
+	 * @throws IndexOutOfBoundsException Σε περίπτωση που ο δείκτης είναι
+	 * μικρότερος του μηδέν ή μεγαλύτερος του μήκους της ουράς
+	 * @see QNode#getNextNode()
+	 */
 	public QNode<E> getNode(int index) throws IndexOutOfBoundsException{
 		if(index<0 || index>length){
 			System.err.println("Index out of bounds");
@@ -67,7 +137,21 @@ public class FifoQueue<E> {
 			return cursor;
 		}
 	}
-	//Removes a node from the queue, not the first one
+	
+	/**
+	 * Παίρνει την τιμή από έναν ενδιάμεσο κόμβο από την ουρά
+	 * και τον αφαιρεί
+	 * 
+	 * @param index Ο δείκτης του κόμβου που μας ενδιαφέρει
+	 * @return Την τιμή του κόμβου που μας ενδιαφέρει
+	 * @throws IndexOutOfBoundsException Σε περίπτωση που ο δείκτης είναι μικρότερος
+	 * του μηδέν ή μεγαλύτερος του μήκους της ουράς
+	 * @see FifoQueue#dequeue()
+	 * @see FifoQueue#getNode(int)
+	 * @see QNode#getNextNode()
+	 * @see QNode#getValue()
+	 * @see QNode#setNextNode(QNode)
+	 */
 	public E removeNode(int index) throws IndexOutOfBoundsException{
 		if(index<0 || index>length){
 			System.err.println("Index out of bounds");
@@ -78,20 +162,35 @@ public class FifoQueue<E> {
 			QNode<E> tmpNode=getNode(index-1);
 			E value=tmpNode.getNextNode().getValue();
 			tmpNode.setNextNode(tmpNode.getNextNode().getNextNode());
+			length--;
 			
 			return value;
 		}
 	}
-	//Give the length of the queue
+
+	/**
+	 * Παίρνουμε το μήκος της ουράς
+	 * 
+	 * @return Το μήκος της ουράς
+	 */
 	public int getLength(){
 		return length;
 	}
-	//Tell us whether the queue is empty
+	
+	/**
+	 * Ελέγχουμε αν η ουρά είναι άδεια
+	 * 
+	 * @return true αν η ουρά είναι άδεια, false αν η ουρά έχει κόμβους
+	 */
 	public boolean isEmpty(){
 		return length==0?true:false;
 	}
-	@Override
-	//Print the queue
+	
+	/**
+	 * Επιστρέφει τις τιμές κάθε κόμβου στην ουρά αναμονής
+	 * 
+	 * @return Τις τιμές κάθε κόμβου στην ουρά αναμονής
+	 */
 	public String toString(){
 		if(length==0){
 			return "Queue is empty!";
