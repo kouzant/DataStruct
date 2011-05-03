c0058ca41c4af0047c1843c0f45698cd2ecf7863
alafroiskiotos <praetorian@akyroi.gr>
2011-05-03 16:43:31 +0300

    documentation of entities package

--- src/entities/Passenger.java
+++ src/entities/Passenger.java
@@ -3,6 +3,11 @@ package entities;
 import java.math.BigInteger;
 import structures.SimplyLinkedList;
 
+/**
+ * Κλάση που κρατάει πληροφορίες για του επιβάτες-κρατήσεις.
+ * Κάθε instance αυτής της κλάσης αποθηκεύεται σε μία διπλά
+ * συνδεδεμένη λίστα.
+ */
 public class Passenger {
 	private String surname;
 	private String name;
@@ -11,10 +16,23 @@ public class Passenger {
 	private String address;
 	private BigInteger phone;
 	private String uid;
-	//True is boarding, false is pending
+	//true αν ο επιβάτης είναι σε λίστα επιβίβασης,
+	//false αν ο επιβάτης είναι σε ουρά αναμονής
 	private boolean status;
+	//Λίστα με τους κωδικούς πτήσεων που έχει κάνει κράτηση ο επιβάτης
 	private SimplyLinkedList<String> bookedFlightsList;
 	
+	/**
+	 * Constructor για τη δημιουργία ενός νέου instance επιβάτη-κράτησης.
+	 * 
+	 * @param surname Το επίθετο του επιβάτη.
+	 * @param name Το όνομα του επιβάτη.
+	 * @param idNumber Ο αριθμός ταυτότητας του επιβάτη.
+	 * @param nationality Η εθνικότητα του επιβάτη.
+	 * @param address Η διεύθυνση του επιβάτη. 
+	 * @param phone Το τηλέφωνο του επιβάτη.
+	 * @param uid Το μοναδικό αναγνωριστικό της κράτησης.
+	 */
 	public Passenger(String surname, String name, String idNumber, String nationality,
 			String address, BigInteger phone, String uid){
 		this.surname=surname;
@@ -24,44 +42,106 @@ public class Passenger {
 		this.address=address;
 		this.phone=phone;
 		this.uid=uid;
+		//Δημιουργία της λίστας με τους κωδικούς πτήσης που έχει κάνει
+		//κράτηση ο επιβάτης
 		bookedFlightsList=new SimplyLinkedList<String>();
 	}
-	
+	/**
+	 * Παίρνουμε το επίθετο του επιβάτη.
+	 * 
+	 * @return Το επίθετο του επιβάτη.
+	 */
 	public String getSurname(){
 		return surname;
 	}
+	/**
+	 * Παίρνουμε το όνομα του επιβάτη.
+	 * 
+	 * @return Το όνομα του επιβάτη.
+	 */
 	public String getName(){
 		return name;
 	}
+	/**
+	 * Παίρνουμε τον αριθμό ταυτότητας του επιβάτη.
+	 * 
+	 * @return Τον αριθμό ταυτότητας του επιβάτη.
+	 */
 	public String getIdNumber(){
 		return idNumber;
 	}
+	/**
+	 * Παίρνουμε την εθνικότητα του χρήστη.
+	 * 
+	 * @return Την εθνικότητα του χρήστης.
+	 */
 	public String getNationality(){
 		return nationality;
 	}
+	/**
+	 * Παίρνουμε τη διεύθυνση του χρήστη.
+	 * 
+	 * @return Τη διεύθυνση του χρήστη.
+	 */
 	public String getAddress(){
 		return address;
 	}
+	/**
+	 * Παίρνουμε το τηλέφωνο του χρήστη.
+	 * 
+	 * @return Το τηλέφωνο του χρήστη.
+	 */
 	public BigInteger getPhone(){
 		return phone;
 	}
+	/**
+	 * Παίρνουμε το μοναδικό αναγνωριστικό της κράτησης.
+	 * 
+	 * @return Το μοναδικό αναγνωριστικό της κράτησης.
+	 */
 	public String getUid(){
 		return uid;
 	}
+	/**
+	 * Παίρνουμε την κατάσταση κράτησης του επιβάτη.
+	 * 
+	 * @return Την κατάσταση κράτησης του επιβάτη.
+	 */
 	public boolean getStatus(){
 		return status;
 	}
+	/**
+	 * Παίρνουμε τη λίστα με τους κωδικούς πτήσεων που έχει κάνει
+	 * κράτηση ο επιβάτης.
+	 * 
+	 * @return Τη λίστα με τους κωδικούς πτήσεων που έχει κάνει
+	 * κράτηση ο επιβάτης.
+	 */
 	public SimplyLinkedList<String> getBookedFlights(){
 		return bookedFlightsList;
 	}
+	/**
+	 * Προσθέτουμε ένα νέο κωδικό πτήσης στην υπάρχουσα λίστα κράτησης.
+	 * 
+	 * @param flightCode Ο κωδικός πτήσης.
+	 */
 	public void setBookedFlights(String flightCode){
 		bookedFlightsList.addTail(flightCode);
 	}
+	/**
+	 * Ορίζουμε την κατάσταση κράτησης του επιβάτη.
+	 * 
+	 * @param status Η κατάσταση κράτησης του επιβάτη.
+	 */
 	public void setStatus(boolean status){
 		this.status=status;
 	}
+	/**
+	 * Επιστρέφει όλες τις λεπτομέρειες ενός επιβάτη-κράτησης.
+	 * 
+	 * @return Τις λεπτομέρειες ενός επιβάτη-κράτησης.
+	 */
 	@Override
-	//Print a passenger's details
 	public String toString(){
 		StringBuilder sb=new StringBuilder();
 		sb.append("\n");
